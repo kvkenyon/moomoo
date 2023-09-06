107b73a7afa9b125a61df182d99e78ea8bd3a904
Kevin Kenyon <kvkenyon@gmail.com>
2023-09-06 14:18:46 -0500

    Create zero tensors of any shape + pretty print

--- moomoo.hs
+++ moomoo.hs
@@ -1,29 +1,32 @@
 module MooMoo where
 
--- f(x1,x2) = ln(x1) + x1*x2 - sin(x2)
+-- Build a computational graph for
+-- a multivariable function of scalars
+-- f(x1, y1) = ln(x1) + x1*x2 - sin(x2)
 
 type Scalar = Double
 
-f :: Scalar -> Scalar -> Scalar
-f x y = log x + (x * y) - sin y
-
-log' :: Scalar -> Scalar
-log' x = 1 / x
-
-mult' :: Scalar -> Scalar -> Scalar
-mult' x y = x
-
-f' :: Scalar -> Scalar -> Scalar
-f' x y = 1 / x + y
-
-newtype Unary = Unary (Scalar -> Scalar)
-
-newtype Binary = Binary (Scalar -> Scalar -> Scalar)
-
-data Vertex = Empty | Vertex Scalar [Vertex]
+data Expr
+  = Val Scalar
+  | Add Expr Expr
+  | Mult Expr Expr
+  | Log Expr
+  | Sin Expr
+  | Subt Expr Expr
   deriving (Show)
 
--- instance Functor ComputeGraph where
---   fmap :: (a -> b) -> ComputeGraph a -> ComputeGraph b
---   fmap f Empty = Empty
---   fmap f (Vertex x) = Vertex $ f x
+value :: Expr -> Scalar
+value (Val x) = x
+value (Add x y) = value x + value y
+value (Mult x y) = value x * value y
+value (Log x) = log $ value x
+value (Sin x) = sin $ value x
+value (Subt x y) = value x - value y
+
+data Op
+  = EVAL Expr
+  | ADD Scalar
+  | MULT Scalar
+  | LOG
+  | SIN
+  deriving (Show)
---
+++ tensor.hs
@@ -0,0 +1,19 @@
+module Tensor where
+
+type Shape = [Int]
+
+type Scalar = Double
+
+data Tensor a = Nil | Val a | T [Tensor a] (Tensor a)
+  deriving (Show)
+
+zeros :: Shape -> Tensor Scalar
+zeros [] = Nil
+zeros [w] = T (replicate w $ Val 0) Nil
+zeros [h, w] = T (replicate h (T (replicate w (Val 0)) Nil)) Nil
+zeros (x : xs) = T (replicate x (zeros xs)) Nil
+
+pretty :: Tensor Scalar -> String
+pretty Nil = ""
+pretty (Val x) = show x ++ " "
+pretty (T vec rest) = concatMap pretty vec ++ "\n" ++ pretty rest
